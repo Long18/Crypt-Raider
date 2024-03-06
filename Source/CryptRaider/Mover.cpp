28f161ea1f88991e634c30821ce38e683d01a6d2
William <us.thanhlong18@gmail.com>
2024-03-06 13:43:38 +0700

    feat: FMath::VInterpConstantTo

--- Source/CryptRaider/Mover.cpp
+++ Source/CryptRaider/Mover.cpp
@@ -2,6 +2,7 @@
 
 
 #include "Mover.h"
+#include "Math/UnrealMathUtility.h"
 
 // Sets default values for this component's properties
 UMover::UMover()
@@ -19,8 +20,7 @@ void UMover::BeginPlay()
 {
 	Super::BeginPlay();
 
-	// ...
-
+	OriginalLocation = GetOwner()->GetActorLocation();
 }
 
 
@@ -29,11 +29,14 @@ void UMover::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponent
 {
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
 
-	AActor* Owner = GetOwner();
-	FString OwnerName = Owner->GetActorNameOrLabel();
-	FVector OwnerLocation = Owner->GetActorLocation();
+	FVector CurrentLocation = GetOwner()->GetActorLocation();
+	FVector TargetLocation = OriginalLocation + MoveOffset;
+	float Speed = FVector::Distance(OriginalLocation, TargetLocation) / MoveTime;
 
-	UE_LOG(LogTemp, Warning, TEXT("Owner: %s"), *OwnerName);
-	UE_LOG(LogTemp, Warning, TEXT("Location: %s"), *OwnerLocation.ToCompactString());
+	FVector NewLocation = FMath::VInterpConstantTo(CurrentLocation, TargetLocation, DeltaTime, Speed);
+
+	if (!ShouldMove) return;
+
+	GetOwner()->SetActorLocation(NewLocation);
 }
 
